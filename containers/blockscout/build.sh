21acad4f53f4ef8ed6696bd191df8412aee4dab4
David Boreham <david@bozemanpass.com>
2025-07-16 19:39:19 -0600

    Fix build script

--- containers/blockscout/build.sh
+++ containers/blockscout/build.sh
@@ -4,4 +4,4 @@ source ${STACK_CONTAINER_BASE_DIR}/build-base.sh
 
 FULL_REPO_PATH=${STACK_REPO_BASE_DIR}/github.com/blockscout/blockscout/
 
-docker build -t ${STACK_IMAGE_NAME}:${STACK_IMAGE_LOCAL_TAG} -f ${FULL_REPO_PATH}/Dockerfile ${build_command_args} ${FULL_REPO_PATH}
+docker build -t ${STACK_IMAGE_NAME}:${STACK_IMAGE_LOCAL_TAG} -f ${FULL_REPO_PATH}/docker/Dockerfile ${build_command_args} ${FULL_REPO_PATH}
