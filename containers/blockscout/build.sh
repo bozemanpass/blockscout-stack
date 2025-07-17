e46cc33a88670cc71269d3b16eb2f23cc32d068c
David Boreham <david@bozemanpass.com>
2025-07-16 19:26:26 -0600

    Initial commit

---
+++ containers/blockscout/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+source ${STACK_CONTAINER_BASE_DIR}/build-base.sh
+# Build a local version of the blockscout image
+
+FULL_REPO_PATH=${STACK_REPO_BASE_DIR}/github.com/blockscout/blockscout/
+
+docker build -t ${STACK_IMAGE_NAME}:${STACK_IMAGE_LOCAL_TAG} -f ${FULL_REPO_PATH}/Dockerfile ${build_command_args} ${FULL_REPO_PATH}
